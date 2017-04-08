54e757dd6d99347e91b6c0315307f069b2b333e6
babatunde360 <babatundeola66@gmail.com>
2017-04-08 11:55:05 +0100

    The javascript code

---
+++ app/library.js
@@ -0,0 +1,16 @@
+'use strict'
+var exports = module.exports = {}
+	exports.fizzBuzz = function(number){
+		if(number % 15 === 0 ){
+			return 'FizzBuzz';
+		}
+		else if(number % 3 === 0){
+			return 'Fizz';
+		}
+		else if(number % 5 === 0){
+			return 'Buzz';
+		}
+		else{
+			return number;
+		}
+	};
